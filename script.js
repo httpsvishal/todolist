91a3457a0362b7c65b604fe9fda6d2ee62addf69
Vishal kumar <vishalkumar756380@gmail.com>
2024-10-25 04:21:28 +0530

    made tasks clickable

--- script.js
+++ script.js
@@ -8,79 +8,114 @@ let checkedUrl = "./assets/checked-logo.svg";
 let allTasks = {};
 let taskId = 1;
 
-createTask = (newTaskName,taskStatusData ="unchecked",id ) =>{
-    if(!id){
+createTask = (newTaskName, taskStatusData = "unchecked", id) => {
+    if (!id) {
         id = taskId;
         taskId++;
     }
     let li = document.createElement("li");
-    li.dataset.id =id;
+    li.dataset.id = id;
 
+    let taskWrapper = document.createElement("span");
+    taskWrapper.classList.add("task-title-wrapper");
     let taskName = document.createElement("span");
     taskName.classList.add("task-title");
     taskName.innerText = newTaskName;
+    taskWrapper.append(taskName);
 
     let taskStatus = document.createElement("img");
     taskStatus.dataset.status = taskStatusData;
-    if(taskStatus.dataset.status == "checked")
+    if (taskStatus.dataset.status == "checked")
         taskStatus.src = checkedUrl;
     else
-    taskStatus.src = uncheckedUrl ;
+        taskStatus.src = uncheckedUrl;
 
     let removeIcon = document.createElement("img");
     removeIcon.src = removeSvgUrl;
 
-    li.append(taskName,taskStatus,removeIcon);
+    li.append(taskWrapper, taskStatus, removeIcon);
     tasksContainer.append(li);
 
     let taskData = {
-        "taskName" : newTaskName, 
+        "taskName": newTaskName,
         status: taskStatusData
     };
-    console.log(taskData,newTaskName);
+    console.log(taskData, newTaskName);
     allTasks[id] = taskData;
-    
+
     //Added EventListeners
-    taskStatus.addEventListener("click",()=>{
+    taskStatus.addEventListener("click", () => {
         console.log("hey");
-        if(taskStatus.dataset.status==="unchecked"){
+        if (taskStatus.dataset.status === "unchecked") {
             taskStatus.dataset.status = "checked";
             taskStatus.src = checkedUrl;
-            allTasks[id].status ="checked";
+            allTasks[id].status = "checked";
         }
-        else{
-            taskStatus.src = uncheckedUrl ;
+        else {
+            taskStatus.src = uncheckedUrl;
             taskStatus.dataset.status = "unchecked";
             allTasks[id].status = "unchecked";
         }
     })
 
-    removeIcon.addEventListener("click",()=>{
+    removeIcon.addEventListener("click", () => {
         li.remove();
         delete allTasks[id];
     })
+
+    let isTask = true;
+    let editTaskForm = document.createElement("form");
+    editTaskForm.classList.add("edit-task-form");
+    let editTaskInput = document.createElement("input");
+    editTaskInput.type = "text";
+    editTaskInput.setAttribute("required", true);
+    editTaskInput.setAttribute("autofocus",true);
+    let editButton = document.createElement("button");
+    editButton.innerText = "Save Changes";
+
+    taskWrapper.addEventListener("click", () => {
+        if (isTask) {
+            isTask = false;
+            let currTaskValue = taskName.innerText;
+            taskName.remove();
+            editTaskInput.value = currTaskValue;
+            editTaskForm.append(editTaskInput, editButton);
+            taskWrapper.append(editTaskForm);
+        }
+    })
+
+    let saveChanges = (e) =>{
+        e.preventDefault();
+        editTaskForm.remove();
+        taskName.innerText = editTaskInput.value;
+        taskWrapper.append(taskName);
+        isTask=true;
+        allTasks[id].taskName = taskName.innerText;
+    }
+
+    editTaskForm.addEventListener("submit",saveChanges);
 }
 
-if(localStorage.getItem("allTasks") !== null){
+if (localStorage.getItem("allTasks") !== null) {
     let allPrevTasks = JSON.parse(localStorage.getItem("allTasks"));
     allkeys = Object.keys(allPrevTasks);
-    if(allkeys.length>0)
-    taskId= Math.max(...allkeys)+1;
+    if (allkeys.length > 0)
+        taskId = Math.max(...allkeys) + 1;
     console.log(taskId)
-    allkeys.forEach((key) =>{
-        createTask(allPrevTasks[key].taskName , allPrevTasks[key].status,key );
+    allkeys.forEach((key) => {
+        createTask(allPrevTasks[key].taskName, allPrevTasks[key].status, key);
     })
 }
 
-const addTask = (e)=>{
+const addTask = (e) => {
     e.preventDefault();
     let newTaskName = newTaskInput.value;
     createTask(newTaskName);
-    newTaskInput.value="";
+    newTaskInput.value = "";
 }
 
-taskForm.addEventListener("submit",addTask);
+taskForm.addEventListener("submit", addTask);
 
-addEventListener("pagehide",()=>{
-    localStorage.setItem("allTasks",JSON.stringify(allTasks));
+addEventListener("pagehide", () => {
+    localStorage.setItem("allTasks", JSON.stringify(allTasks));
 })
